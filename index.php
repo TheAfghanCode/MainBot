2c617cd133db16d49403b68587348a6628b65658
Alisina <developer.alisina@gmail.com>
2025-06-12 14:49:50 +0430

    Start The Project

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$env = parse_ini_file('.env');
+$BOT_TOKEN = $env['BOT_TOKEN'];
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (isset($data['user_id']) && isset($data['message'])) {
+    $chat_id = $data['user_id'];
+    $text = $data['message'];
+
+    // Send message back to user
+    $url = "https://api.telegram.org/bot$BOT_TOKEN/sendMessage";
+    $params = [
+        'chat_id' => $chat_id,
+        'text' => $text
+    ];
+
+    file_get_contents($url . '?' . http_build_query($params));
+
+    // Optional: log the request
+    file_put_contents("log.txt", date('c') . " - Sent to $chat_id: $text\n", FILE_APPEND);
+}
+?>
---
+++ webhook.php
@@ -0,0 +1,32 @@
+<?php
+// Load env variables
+$env = parse_ini_file('.env');
+$BOT_TOKEN = $env['BOT_TOKEN'];
+
+$update = json_decode(file_get_contents('php://input'), true);
+
+if (isset($update['message'])) {
+    $chat_id = $update['message']['chat']['id'];
+
+    // If /start command
+    if ($update['message']['text'] === '/start') {
+        $keyboard = [
+            'keyboard' => [],
+            'inline_keyboard' => [[
+                [
+                    'text' => '🧩 Open Mini App',
+                    'web_app' => ['url' => 'https://your-render-url.com/mini-app/index.html']
+                ]
+            ]]
+        ];
+
+        $reply = [
+            'chat_id' => $chat_id,
+            'text' => "👋 Welcome to Afghan Code!\nClick the button below to open the mini app.",
+            'reply_markup' => json_encode($keyboard)
+        ];
+
+        file_get_contents("https://api.telegram.org/bot$BOT_TOKEN/sendMessage?" . http_build_query($reply));
+    }
+}
+?>
